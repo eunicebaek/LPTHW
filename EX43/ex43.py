e5b8a4702815f28098e7b03e810b23f69ef3879e
eunicebaek <afroginacup@gmail.com>
2016-11-13 21:01:45 +0100

    ex38-44

---
+++ EX43/ex43.py
@@ -0,0 +1,33 @@
+class Death(Scene):
+
+    quips = [
+        "You died.  You kida suck at this."
+        "Your mom would be proud...if she were smarter."
+        "Such a luser."
+        "I have a small puppy that's better at this."
+    ]
+
+    def enter(self):
+        print Death.quips[randint(0, len(self.quips)-1)]
+        exit(1)
+
+class CentralCorridor(Scene):
+
+    def enter(self):
+        print "The Gothons of Planet Percal #25 have invaded your ship and destroyed"
+        print "your entire crew.  You are the last surviving member and your last"
+        print "mission is to get the  neutron destruct bomb from the Weapons Armory,"
+        print "put it in the bridge, and blow the ship up after getting into an"
+        print "escape pod."
+        print "\n"
+        print "You're running down the central corridor to the Weapons Armory when"
+        print "a Gothon jumps out, red scary skin, dark grimy teeth, and evil clown costume"
+        print "flowing around his hate filled body. He's blocking the door to the"
+        prit "Armory and about to pull a weapon to blast you."
+
+        action = raw_input("> ")
+
+        if action == "shoot!":
+            print "Quick on the draw you yank out your blaster and fire it at the Gothon."
+            print "His clown costume is flowing and moving around his body, which throws"
+            print "off your aim.  Your laser hits his costume but misses him entirely."
